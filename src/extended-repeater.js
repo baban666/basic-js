5e5847c9b40be9f8b9e2d533f693fa3a949f62c8
Vladimir Veraks <vladimirveraksa@gmail.com>
2020-03-03 00:06:52 +0300

    feat: add vigenere and repeter

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,41 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+// repeater('STRING', { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
+// =>'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
+
+
+module.exports = function repeater(str, options) {
+    let result = '';
+    let {
+        repeatTimes,
+        separator = '+',
+        addition,
+        additionRepeatTimes,
+        additionSeparator = '|' } = options;
+
+
+    if(repeatTimes === undefined && additionRepeatTimes === undefined){
+
+        return result = `${str}${addition}`;
+    }
+
+    const getAdditionalString = (addition, additionRepeatTimes, additionSeparator) => {
+
+        let AdditionalString = '';
+        while (additionRepeatTimes--){
+            AdditionalString += `${addition}${(additionRepeatTimes == 0) ? '' : additionSeparator}`;
+        }
+        return AdditionalString;
+    };
+
+    const getString = (str, separator, repeatTimes) => {
+        let finalString = '';
+        while (repeatTimes--){
+            finalString += `${str}${getAdditionalString(addition, additionRepeatTimes, additionSeparator)}${(repeatTimes == 0) ? '' : separator}`;
+        }
+        return finalString;
+    };
+
+    return getString(str, separator, repeatTimes);
 };
-  
+
+// console.log(repeater('STRING', { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' }))
+// console.log('STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS')
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,70 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(inverted = true) {
+        this.inverted = inverted;
+        this.tabula = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        this.position = 0;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+    encrypt(str, key) {
+        let result = '';
+
+        if (!str || !key){
+            throw new Error();
+        }
+
+       let msg = str.toUpperCase();
+       key = key.toUpperCase();
+
+        while (key.length < msg.length) {
+            key += key;
+        }
+
+        for (let i = 0, j = 0; i < msg.length; i++ , j++) {
+            if (this.tabula.includes(msg[i])) {
+                this.position = this.tabula.indexOf(msg[i]) + this.tabula.indexOf(key[j])
+                if (this.position >= this.tabula.length) {
+                    this.position -= this.tabula.length;
+                }
+                result += this.tabula[this.position];
+            } else {
+                result += msg[i];
+                j--;
+            }
+        }
+
+        return (this.inverted) ? result : result.split('').reverse().join('')
+    };
+
+    decrypt(str, key) {
+        let result = '';
+
+        if (!str || !key){
+            throw new Error();
+        }
+
+        let msg = str.toUpperCase();
+        key = key.toUpperCase();
+
+        while (key.length < msg.length) {
+            key += key;
+        }
+
+        for (let i = 0, j = 0; i < msg.length; i++ , j++) {
+            if (this.tabula.includes(msg[i])) {
+                this.position = this.tabula.indexOf(msg[i]) - this.tabula.indexOf(key[j])
+                if (this.position < 0) {
+                    this.position += this.tabula.length;
+                }
+                result += this.tabula[this.position];
+            } else {
+                result += msg[i];
+                j--;
+            }
+        }
+
+        return (this.inverted) ? result : result.split('').reverse().join('')
+    };
+
 }
 
 module.exports = VigenereCipheringMachine;
